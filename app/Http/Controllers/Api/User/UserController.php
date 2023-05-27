238dafdbc44f720f174a9a05c73160ace39efec2
SalahCheihbi <cheihbisalah@gmail.com>
2023-05-27 21:26:41 +0100

    register api

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Traits\GeneralTrait;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -38,18 +40,44 @@ class UserController extends Controller
             $user->api_token = $token;
             //token
             return $this->returnData('user', $user, msg: "login successfully");
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
+            return $this->returnError($ex->getMessage(), $ex->getCode());
+        }
+    }
+    public function register(Request $request)
+    {
+        try {
+            $rules = [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6'
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                $code = $this->returnCodeAccordingToInput($validator);
+                return $this->returnValidationError($code, $validator);
+            }
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => bcrypt($request->input('password')),
+            ]);
+            $token = Auth::guard('user-api')->login($user);
+            $user->api_token = $token;
+
+            return $this->returnData('user', $user, 'Registration successful');
+        } catch (Exception $ex) {
             return $this->returnError($ex->getMessage(), $ex->getCode());
         }
     }
     public function logout(Request $request)
     {
 
-         $token = $request->header('auth-token');
+        $token = $request->header('auth-token');
         if ($token) {
             JWTAuth::setToken($token)->invalidate();
 
-            return $this -> returnSuccessMessage(msg: "logout successfully");
+            return $this->returnSuccessMessage(msg: "logout successfully");
         } else {
             $this->returnError('E001', 'some thing went wrong');
         }
--- routes/api.php
+++ routes/api.php
@@ -2,40 +2,31 @@
 
 use App\Http\Controllers\Api\Admin\AuthController;
 use App\Http\Controllers\Api\CategoriesController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
 
 Route::group(['middleware' => ['api', 'checkPassword', 'changeLang'], 'namespace' => 'Api'], function () {
     Route::post('/get-main-categories', [CategoriesController::class, 'index']);
     Route::post('/get-category-byId', [CategoriesController::class, 'getCategoryById']);
     Route::post('change-category-status', [CategoriesController::class, 'changeStatus']);
-
     Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
         Route::post('/login', [AuthController::class, 'login']);
-
-        Route::post('logout', [AuthController::class, 'logout']) -> middleware(['auth.guard:admin-api']);
-
-
-
-
+        Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth.guard:admin-api']);
         //invalidate the token  security side
 
         //broken access controller user enumeration
-
     });
-    Route::group((['prefix' =>'user', 'namespace'=> 'User']), function(){
+    Route::group((['prefix' => 'user', 'namespace' => 'User']), function () {
         Route::post('login', [UserController::class, 'login']);
-
+        Route::post('register', [UserController::class, 'register']);
     });
-    Route::group((['prefix' =>'user', 'middleware' => 'auth.guard:user-api']), function(){
-        Route::post('profile', function(){
+    Route::group((['prefix' => 'user', 'middleware' => 'auth.guard:user-api']), function () {
+        Route::post('profile', function () {
             return Auth::user();
         });
     });
 });
-
 Route::group(['middleware' => ['api', 'checkPassword', 'changeLang', 'checkAdminToken:admin-api'], 'namespace' => 'Api'], function () {
     Route::get('offers', [AuthController::class, 'index']);
 });
